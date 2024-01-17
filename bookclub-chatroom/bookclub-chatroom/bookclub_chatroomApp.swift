a3178eafc647b139305f4d8e307c142df52ff4a6
TiannaLopes <tiannaalinalopes@gmail.com>
2024-01-17 09:03:47 -0500

    Initializing project

---
+++ bookclub-chatroom/bookclub-chatroom/bookclub_chatroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookclub_chatroomApp.swift
+//  bookclub-chatroom
+//
+//  Created by Tianna Alina Lopes on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct bookclub_chatroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bookclub-chatroom/bookclub-chatroomUITests/bookclub_chatroomUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bookclub_chatroomUITestsLaunchTests.swift
+//  bookclub-chatroomUITests
+//
+//  Created by Tianna Alina Lopes on 1/16/24.
+//
+
+import XCTest
+
+final class bookclub_chatroomUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
